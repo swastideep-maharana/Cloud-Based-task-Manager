732c50583dcc89ee57fc1dc47e8a639d58f449fe
swastideep maharana <swastideep67maharana@gmail.com>
2024-10-13 08:01:57 +0530

    #added the loginpage ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,39 @@
 import Login from "./pages/Login";
-import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
-import Dashborad from "./pages/Dashboard";
-import Tasks from "./pages/Tasks";
-import Users from "./pages/Users";
-import Trash from "./pages/Trash";
-import TaskDetails from "./pages/TaskDetails";
+import {
+  Routes,
+  Route,
+  Navigate,
+  replace,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
+import Dashborad from "./pages/Dashboard.jsx";
+import Tasks from "./pages/Tasks.jsx";
+import Users from "./pages/Users.jsx";
+import Trash from "./pages/Trash.jsx";
+import TaskDetails from "./pages/TaskDetails.jsx";
+import { Toaster } from "sonner";
 
+function Layout() {
+  const user = "";
+
+  const location = useLocation();
+
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:felx-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* <Sidevar/> */}
+      </div>
+      {/* <MobileSideBar/> */}
+      <div className="flex-1 overflow-y-auto">{/* <Navbar/> */}</div>
+      <div className="p-4 2xl:p-10">
+        <Outlet />
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/log-in" state={{ from: location }} replace />
+  );
+}
 function App() {
   return (
     <main className="w-full min-h-screen bg-[#f3f4f6]">
@@ -24,6 +52,7 @@ function App() {
 
         <Route path="/log-in" element={<Login />} />
       </Routes>
+      <Toaster richColor />
     </main>
   );
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,50 @@
-import React from "react";
-import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 const Login = () => {
+  const user = "";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    user && navigate("/dashboard");
+  }, [user]);
+
   return (
-    <div>
-      
+    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
+      <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
+        {/* Left Side */}
+        <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+          <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
+            <span className="flex gap-1 py-1 px-4 border rounded-full text-sm md:text-base border-gray-300 text-gray-600">
+              Manage all your tasks in one place!
+            </span>
+            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
+              <span>Cloud-Based</span>
+              <span>Task Manager</span>
+            </p>
+
+            <div className="cell">
+              <div className="circle rotate-in-up-left"></div>
+            </div>
+          </div>
+        </div>
+        {/* Right Side */}
+        <div></div>
+      </div>
     </div>
   );
 };
