49e201255dbbe69c3ab9ff825dbef125185212a9
swastideep maharana <swastideep67maharana@gmail.com>
2024-10-16 01:51:41 +0530

    #added the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,173 @@
-import React from 'react'
+import React from "react";
+import {
+  MdAdminPanelSettings,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+  MdKeyboardDoubleArrowUp,
+} from "react-icons/md";
+import { LuClipboardEdit } from "react-icons/lu";
+import { FaNewspaper } from "react-icons/fa";
+import { FaArrowsToDot } from "react-icons/fa6";
+import moment from "moment";
+import clsx from "clsx"; // Ensure clsx is imported
+import { summary } from "../assets/data";
+import Chart from "../components/Chart";
 
-const Dashboard = () => {
+// TableRow Component
+const TableRow = ({ task }) => {
+  const ICONS = {
+    high: <MdKeyboardDoubleArrowUp />,
+    medium: <MdKeyboardArrowUp />,
+    low: <MdKeyboardArrowDown />,
+  };
+
   return (
-    <div>Dashboard</div>
-  )
-}
+    <tr className="border-b border-gray-300">
+      <td className="py-2">{task.title}</td>
+      <td className="py-2 flex items-center">
+        {ICONS[task.priority]}
+        <span className="ml-2 capitalize">{task.priority}</span>
+      </td>
+      <td className="py-2">{task.team}</td>
+      <td className="py-2 hidden md:block">
+        {moment(task.createdAt).format("MMMM Do, YYYY")}
+      </td>
+    </tr>
+  );
+};
 
-export default Dashboard
+// TaskTable Component
+const TaskTable = ({ tasks }) => {
+  const TableHeader = () => (
+    <thead className="border-b border-gray-300">
+      <tr className="text-black text-left">
+        <th className="py-2">Task Title</th>
+        <th className="py-2">Priority</th>
+        <th className="py-2">Team</th>
+        <th className="py-2 hidden md:block">Created At</th>
+      </tr>
+    </thead>
+  );
+
+  const TableRow = ({ task }) => (
+    <tr className="border-b border-gray-200  text-gray-600 hover:bg-gray-400/10">
+      <td className="py-2">
+        <div className="flex items-center gap-3">
+          <div className="w-9 h-9 rounded-full text-white flex items-center justify-center text-sm bg-violet-700">
+            <span className="text-center">{getInitials(user?.name)}</span>
+          </div>
+
+          <div>
+            <p> {user.name}</p>
+            <span className="text-xs text-black">{user?.role}</span>
+          </div>
+        </div>
+      </td>
+      <td>
+        <p
+          className={clsx(
+            "w-fit px-3 py-1 rounded-full text-sm",
+            user?.isActive ? "bg-blue-200" : "bg-yellow-100"
+          )}
+        >
+          {user?.isActive ? "Active" : "Disabled"}
+        </p>
+      </td>
+      <td className="py-2 text-sm">{moment(user?.createdAt).fromNow()}</td>
+    </tr>
+  );
+
+  return (
+    <div className="w-full md:w-1/3 bg-white h-fit px-2 md:px-6 py-4 shadow-md rounded">
+      <table className="w-full mb-5">
+        <TableHeader />
+        <tbody>
+          {tasks?.map((task, index) => (
+            <TableRow key={index + task._id} task={task} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+// Card Component
+const Card = ({ label, count, icon, bg }) => {
+  return (
+    <div className="w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between">
+      <div className="h-full flex flex-1 flex-col justify-between">
+        <p className="text-base text-gray-600">{label}</p>
+        <span className="text-2xl font-semibold">{count}</span>
+        <span className="text-sm text-gray-400">{"110 last month"}</span>
+      </div>
+
+      <div
+        className={clsx(
+          "w-10 h-10 rounded-full flex items-center justify-center text-white",
+          bg
+        )}
+      >
+        {icon}
+      </div>
+    </div>
+  );
+};
+
+// Dashboard Component
+const Dashboard = () => {
+  const totals = summary.totals;
+
+  const stats = [
+    {
+      _id: "1",
+      label: "TOTAL TASKS",
+      total: summary?.totalTasks || 0,
+      icon: <FaNewspaper />,
+      bg: "bg-[#1d4ed8]",
+    },
+    {
+      _id: "2",
+      label: "COMPLETED TASKS",
+      total: totals["completed"] || 0,
+      icon: <MdAdminPanelSettings />,
+      bg: "bg-[#0f766e]",
+    },
+    {
+      _id: "3",
+      label: "TASK IN PROGRESS",
+      total: totals["in-progress"] || 0,
+      icon: <LuClipboardEdit />,
+      bg: "bg-[#f59e0b]",
+    },
+    {
+      _id: "4",
+      label: "TODOS",
+      total: totals["todo"] || 0,
+      icon: <FaArrowsToDot />,
+      bg: "bg-[#be185d]",
+    },
+  ];
+
+  return (
+    <div className="h-full py-4">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
+        {stats.map(({ icon, bg, label, total }, index) => (
+          <Card key={index} icon={icon} bg={bg} label={label} count={total} />
+        ))}
+      </div>
+      <div className="w-full bg-white my-16 p-4 rounded shadow-sm">
+        <h4 className="text-xl text-gray-600 font-semibold">
+          Chart by Priority
+        </h4>
+        <Chart />
+      </div>
+      <div className="w-full flex flex-col md:flex-row gap-4 2xl:gap-10 py-8">
+        {/* Left - Task Table */}
+        <TaskTable tasks={summary.last10Task} />
+        {/* Right */}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
