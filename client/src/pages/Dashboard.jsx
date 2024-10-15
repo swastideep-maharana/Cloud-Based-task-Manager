991d7b7a80323f8f5f374d12149f0fe95377f8ef
swastideep maharana <swastideep67maharana@gmail.com>
2024-10-16 03:37:43 +0530

    #added the userinfo to the components and added in to the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,40 +6,25 @@ import {
   MdKeyboardDoubleArrowUp,
 } from "react-icons/md";
 import { LuClipboardEdit } from "react-icons/lu";
-import { FaNewspaper } from "react-icons/fa";
+import { FaNewspaper, FaUsers } from "react-icons/fa";
 import { FaArrowsToDot } from "react-icons/fa6";
 import moment from "moment";
-import clsx from "clsx"; // Ensure clsx is imported
 import { summary } from "../assets/data";
+import clsx from "clsx";
 import Chart from "../components/Chart";
 
-// TableRow Component
-const TableRow = ({ task }) => {
+import { BGS, PRIOTITYSTYELS, TASK_TYPE, getInitials } from "../utils";
+import UserInfo from "../components/UserInfo";
+
+const TaskTable = ({ tasks }) => {
   const ICONS = {
     high: <MdKeyboardDoubleArrowUp />,
     medium: <MdKeyboardArrowUp />,
     low: <MdKeyboardArrowDown />,
   };
 
-  return (
-    <tr className="border-b border-gray-300">
-      <td className="py-2">{task.title}</td>
-      <td className="py-2 flex items-center">
-        {ICONS[task.priority]}
-        <span className="ml-2 capitalize">{task.priority}</span>
-      </td>
-      <td className="py-2">{task.team}</td>
-      <td className="py-2 hidden md:block">
-        {moment(task.createdAt).format("MMMM Do, YYYY")}
-      </td>
-    </tr>
-  );
-};
-
-// TaskTable Component
-const TaskTable = ({ tasks }) => {
   const TableHeader = () => (
-    <thead className="border-b border-gray-300">
+    <thead className="border-b border-gray-300 ">
       <tr className="text-black text-left">
         <th className="py-2">Task Title</th>
         <th className="py-2">Priority</th>
@@ -50,7 +35,76 @@ const TaskTable = ({ tasks }) => {
   );
 
   const TableRow = ({ task }) => (
-    <tr className="border-b border-gray-200  text-gray-600 hover:bg-gray-400/10">
+    <tr className="border-b border-gray-300 text-gray-600 hover:bg-gray-300/10">
+      <td className="py-2">
+        <div className="flex items-center gap-2">
+          <div
+            className={clsx("w-4 h-4 rounded-full", TASK_TYPE[task.stage])}
+          />
+
+          <p className="text-base text-black">{task.title}</p>
+        </div>
+      </td>
+
+      <td className="py-2">
+        <div className="flex gap-1 items-center">
+          <span className={clsx("text-lg", PRIOTITYSTYELS[task.priority])}>
+            {ICONS[task.priority]}
+          </span>
+          <span className="capitalize">{task.priority}</span>
+        </div>
+      </td>
+
+      <td className="py-2">
+        <div className="flex">
+          {task.team.map((m, index) => (
+            <div
+              key={index}
+              className={clsx(
+                "w-7 h-7 rounded-full text-white flex items-center justify-center text-sm -mr-1",
+                BGS[index % BGS.length]
+              )}
+            >
+              <UserInfo user={m} />
+            </div>
+          ))}
+        </div>
+      </td>
+      <td className="py-2 hidden md:block">
+        <span className="text-base text-gray-600">
+          {moment(task?.date).fromNow()}
+        </span>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="w-full md:w-2/3 bg-white px-2 md:px-4 pt-4 pb-4 shadow-md rounded">
+      <table className="w-full">
+        <TableHeader />
+        <tbody>
+          {tasks?.map((task, id) => (
+            <TableRow key={id} task={task} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const UserTable = ({ users }) => {
+  const TableHeader = () => (
+    <thead className="border-b border-gray-300 ">
+      <tr className="text-black text-left">
+        <th className="py-2">Full Name</th>
+        <th className="py-2">Status</th>
+        <th className="py-2">Created At</th>
+      </tr>
+    </thead>
+  );
+
+  const TableRow = ({ user }) => (
+    <tr className="border-b border-gray-200 text-gray-600 hover:bg-gray-400/10">
       <td className="py-2">
         <div className="flex items-center gap-3">
           <div className="w-9 h-9 rounded-full text-white flex items-center justify-center text-sm bg-violet-700">
@@ -58,11 +112,12 @@ const TaskTable = ({ tasks }) => {
           </div>
 
           <div>
-            <p> {user.name}</p>
+            <p>{user.name}</p>
             <span className="text-xs text-black">{user?.role}</span>
           </div>
         </div>
       </td>
+
       <td>
         <p
           className={clsx(
@@ -82,8 +137,8 @@ const TaskTable = ({ tasks }) => {
       <table className="w-full mb-5">
         <TableHeader />
         <tbody>
-          {tasks?.map((task, index) => (
-            <TableRow key={index + task._id} task={task} />
+          {users?.map((user, index) => (
+            <TableRow key={index + user?._id} user={user} />
           ))}
         </tbody>
       </table>
@@ -91,62 +146,60 @@ const TaskTable = ({ tasks }) => {
   );
 };
 
-// Card Component
-const Card = ({ label, count, icon, bg }) => {
-  return (
-    <div className="w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between">
-      <div className="h-full flex flex-1 flex-col justify-between">
-        <p className="text-base text-gray-600">{label}</p>
-        <span className="text-2xl font-semibold">{count}</span>
-        <span className="text-sm text-gray-400">{"110 last month"}</span>
-      </div>
-
-      <div
-        className={clsx(
-          "w-10 h-10 rounded-full flex items-center justify-center text-white",
-          bg
-        )}
-      >
-        {icon}
-      </div>
-    </div>
-  );
-};
-
-// Dashboard Component
 const Dashboard = () => {
-  const totals = summary.totals;
+  const totals = summary.tasks;
 
   const stats = [
     {
       _id: "1",
-      label: "TOTAL TASKS",
+      label: "TOTAL TASK",
       total: summary?.totalTasks || 0,
       icon: <FaNewspaper />,
       bg: "bg-[#1d4ed8]",
     },
     {
       _id: "2",
-      label: "COMPLETED TASKS",
+      label: "COMPLETED TASK",
       total: totals["completed"] || 0,
       icon: <MdAdminPanelSettings />,
       bg: "bg-[#0f766e]",
     },
     {
       _id: "3",
-      label: "TASK IN PROGRESS",
-      total: totals["in-progress"] || 0,
+      label: "TASK IN PROGRESS ",
+      total: totals["in progress"] || 0,
       icon: <LuClipboardEdit />,
       bg: "bg-[#f59e0b]",
     },
     {
       _id: "4",
       label: "TODOS",
-      total: totals["todo"] || 0,
+      total: totals["todo"],
       icon: <FaArrowsToDot />,
       bg: "bg-[#be185d]",
     },
   ];
+
+  const Card = ({ label, count, bg, icon }) => {
+    return (
+      <div className="w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between">
+        <div className="h-full flex flex-1 flex-col justify-between">
+          <p className="text-base text-gray-600">{label}</p>
+          <span className="text-2xl font-semibold">{count}</span>
+          <span className="text-sm text-gray-400">{"110 last month"}</span>
+        </div>
+
+        <div
+          className={clsx(
+            "w-10 h-10 rounded-full flex items-center justify-center text-white",
+            bg
+          )}
+        >
+          {icon}
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="h-full py-4">
@@ -155,16 +208,17 @@ const Dashboard = () => {
           <Card key={index} icon={icon} bg={bg} label={label} count={total} />
         ))}
       </div>
+
       <div className="w-full bg-white my-16 p-4 rounded shadow-sm">
         <h4 className="text-xl text-gray-600 font-semibold">
           Chart by Priority
         </h4>
         <Chart />
       </div>
+
       <div className="w-full flex flex-col md:flex-row gap-4 2xl:gap-10 py-8">
-        {/* Left - Task Table */}
         <TaskTable tasks={summary.last10Task} />
-        {/* Right */}
+        <UserTable users={summary.users} />
       </div>
     </div>
   );
